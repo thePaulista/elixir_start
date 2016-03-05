6ab778cc1af77e72d790ae1f6f9b722273c23d92
Hedy <hedywoo@gmail.com>
2016-03-04 23:00:05 -0600

    let's start with a hello

---
+++ hello.exs
@@ -0,0 +1,5 @@
+IO.puts "Ola!"
+
+# extension .ex for programs that you intend to compile into  binary form, and .exs for scripts that you want to run iwthout compiling.
+
+# this one is a throw away code. 
